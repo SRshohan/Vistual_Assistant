3830532d95322201bab19ad37b66671148c2cf11
SRshohan <srshohan02@gmail.com>
2023-11-10 12:02:23 -0500

    Inport libraries

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import json
+import torch
 
+from transformers import GPT2ForSequenceClassification, GPT2Tokenizer, DistilBertForSequenceClassification, DistilBertForTokenClassification
 import speech_recognition as sr
 from gtts import gTTS
+import os
+
+
 
 # obtain audio from the microphone
 r = sr.Recognizer()
@@ -29,7 +35,43 @@ def wakeUpAI():
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-    tts_en = gTTS(speech, lang='en')
+    tts_en = gTTS(speech, lang='bg')
+    tts_en.save('speech.mp3')
+
+    # # Get the current working directory
+    # current_dic = os.getcwd()
+    #
+    # # Construct the model path in the current directory
+    # model_path = os.path.join(current_dic, "My_first_Model")
+
+
+    model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+    tokenizeR = DistilBertForTokenClassification.from_pretrained("distilbert-base-uncased")
+
+    #Load JSON file
+    with open('intents.json', 'r') as file:
+        data = json.load(file)
+
+    for entry in data:
+        # Tokenize the input text
+        input_tokens = tokenizeR(entry['patterns'], return_tensons='pt')
+
+        #Make a prediction
+        with torch.no_grad():
+            outputs = model(**input_tokens)
+
+        #Get predicted probabilities
+        probs = torch.nn.functional.softmax(outputs.logits, dim=-1).squeeze().tolist()
+
+        #Get the predicted intent
+        predicted_intent_index = torch.argmax(outputs.logits).item()
+        predicted_intent = model.config.id2label[predicted_intent_index]
+
+        # Visualize the results
+        print("Input Text:", entry['text'])
+        print("Predicted Intent:", predicted_intent)
+        print("Predicted Probabilities:", {label: prob for label, prob in zip(model.config.id2label.values(), probs)})
+        print("-----")
 
 
 wakeUpAI()
